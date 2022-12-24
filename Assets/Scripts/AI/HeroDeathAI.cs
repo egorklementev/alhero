2e97429bee3b9c29974406990f24aeedd3f39dfd
Egor Klementev <egorklementeff@yandex.ru>
2022-12-24 17:54:17 +0300

    FEATURE: items drop when death

--- Assets/Scripts/AI/HeroDeathAI.cs
+++ Assets/Scripts/AI/HeroDeathAI.cs
@@ -1,9 +1,22 @@
+using UnityEngine;
+
 public class HeroDeathAI : SomeAI
 {
     public override void PrepareAction()
     {
         UIController.HideActiveGroups();
         HeroMoveController.uiTookControl = true;
+
+        foreach (ItemWorld item in LogicController.PickedItems)
+        {
+            var randomDirection = Random.onUnitSphere * 5f;
+            randomDirection.y = Mathf.Abs(randomDirection.y);
+            if (item != null)
+            {
+                item.SetPickedUp(false);
+                item.GetBody().AddRelativeForce(randomDirection, ForceMode.Impulse);
+            }
+        }
     }
 
     public override void Act()
